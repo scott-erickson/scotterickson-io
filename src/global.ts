88683ab116eff1a0f59bf9c372d77410eeb5e71a
Scott Erickson <sryanerickson@gmail.com>
2023-05-09 16:57:07 -0600

    config luxy js

--- src/global.ts
+++ src/global.ts
@@ -1,4 +1,14 @@
+import luxy from 'luxy.js';
+
 window.Webflow ||= [];
 window.Webflow.push(() => {
   console.log('hello global script!');
 });
+
+const isMobile = /iPhone|iPad|Android/i.test(navigator.userAgent);
+if (!isMobile) {
+  luxy.init({
+    wrapper: '#luxy',
+    wrapperSpeed: 0.0865,
+  });
+}
