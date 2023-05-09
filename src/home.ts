458df770c7d40cd9db1be659b5b5d0544e52b226
Scott Erickson <sryanerickson@gmail.com>
2023-05-09 17:01:00 -0600

    add react packages

--- src/home.ts
+++ src/home.ts
@@ -1,15 +1,5 @@
-import luxy from 'luxy.js';
-
 console.log('hi');
 
 const fireRoot = document.querySelector('#fire-root');
 
 console.log(fireRoot);
-
-const isMobile = /iPhone|iPad|Android/i.test(navigator.userAgent);
-if (!isMobile) {
-  luxy.init({
-    wrapper: '#luxy',
-    wrapperSpeed: 0.0865,
-  });
-}
